6016d0be469614629cfca5d23ea90436936cd40a
Benjamin Orgis <mail@orgis.be>
2017-06-30 16:42:35 +0200

    mudularization of scripts into files

---
+++ functions.inc.php
@@ -0,0 +1,138 @@
+<?php
+function price($diff) {
+	global $deposit;
+	
+	$fullDays = floor($diff / 86400);
+	$leftMinutes = floor($diff / 60) - ($fullDays * 1440);
+
+	// TAGESPREIS: 3,00 € pro vollem Tag
+	$daysPrice = $fullDays*3;
+
+	// STUNDENPREIS: 0,39 € pro Stunde + ggf. 0,90 € Grundgebühr
+	$minutesPrice = $leftMinutes * (.39 / 60);
+	if($fullDays == 0) $minutesPrice += .9;
+	if($minutesPrice > 3) $minutesPrice = 3;
+
+	// GESAMTPREIS
+	$price = $daysPrice + $minutesPrice;
+
+	// KAUTION prüfen
+	if ($price > $deposit) $price = $deposit;
+
+	return round($price, 2);
+}
+
+function loadData() {
+	$arr = @json_decode(file_get_contents('data/'.USER.'.json'), true);
+	if (!$arr) $arr = [
+		'lend' => [],
+		'history' => []
+	];
+	return $arr;
+}
+
+function cronMove2History(&$myData) {
+	$myDataChanged = false;
+	$maxAge = time() - 3600 * 24 * 7;
+	foreach ($myData['lend'] as $i => $v) {
+		if ($v['ts'] < $maxAge) {
+			// move to history
+			$myData['history'][] = $v + [
+					'rdate' => 'Nie',
+					'rtime' => '',
+					'price' => $deposit,
+					'cable' => 'no',
+					'adapter' => 'no'
+				];
+			unset($myData['lend'][$i]);
+			$myDataChanged = true;
+		}
+	}
+	if ($myDataChanged) saveData($myData);
+}
+
+function saveData($myData) {
+	file_put_contents('data/'.USER.'.json', json_encode($myData, JSON_PRETTY_PRINT));
+}
+
+### AJAX
+function addLend($myData, $id, $device) {
+	global $deposit;
+	
+	if ($id) {
+		$json = [
+			'id' => $id,
+			'device' => $device,
+			'date' => date('d.m.y'),
+			'time' => date('H:i'),
+			'ts' => time()
+		];
+		$myData['lend'][] = $json;
+		$myDataChanged = true;
+		$json['success'] = true;
+		saveData($myData);
+	} else {
+		$json = ['error' => 'Fehlerhafte/Fehlende Formularinformationen erhalten'];
+	}
+	return $json;
+}
+
+function calculatePrice($myData, $id) {
+	foreach ($myData['lend'] as $k => $lend) {
+		if ($lend['id'] == $id) {
+			$to = time();
+			$price = price($to - $lend['ts']);
+			$json = [
+				'price' => floor($price * 100) / 100,
+				'time' => $to,
+				'device' => $lend['device']
+			];
+			$found = true;
+			break;
+		}
+	}
+	if ($found) $json['success'] = true;
+	else $json['error'] = 'Akku-Nr. konnte nicht ermittelt werden.';
+	return $json;
+}
+
+function returned($myData, $id, $cable, $adapter, $toReceived) {
+	global $deposit;
+	
+	$error = 'AkkuNr konnte nicht gefunden werden. Bitte aktualisieren Sie die Seite und versuchen Sie es erneut.';
+	if ($id) {
+		$found = false;
+		foreach ($myData['lend'] as $k => $lend) {
+			if ($lend['id'] == $id) {
+				if (time() - $toReceived > 60 * 12) {
+					$error = 'Zeit abgelaufen, versuchen Sie es erneut.';
+					break;
+				}
+				$price = price($toReceived - $lend['ts']);
+				if ($lend['device']) {
+					if (!$cable) $price += 2;
+					if ($lend['device'] > 1 && !$adapter) $price += 1.5;
+				}
+				if ($price > $deposit) $price = $deposit;
+				$d = $lend + [
+						'rdate' => date('d.m.y', $toReceived), 'rtime' => date('H:i', $toReceived),
+						'rts' => $toReceived,
+						'price' => number_format(floor($price * 100) / 100, 2, ',', ' '),
+						'cable' => $cable ? 'yes' : 'no',
+						'adapter' => $adapter ? 'yes' : 'no'
+					];
+				$myData['history'][] = $d;
+				$json = $d;
+				unset($myData['lend'][$k]);
+				saveData($myData);
+				$found = true;
+				break;
+			}
+		}
+		if ($found) $json['success'] = true;
+		else $json = ['error' => $error];
+	} else {
+		$json = ['error' => 'AkkuNr konnte nicht ermittelt werden'];
+	}
+	return $json;
+}
---
+++ script.js
@@ -0,0 +1,211 @@
+var $lendTable = $('#lend-table'),
+	$historyTable = $('#history-table'),
+	$dialogs = $('.mdl-dialog'),
+	returnTo = false;
+$dialogs.each(function() {
+	dialogPolyfill.registerDialog(this);
+	var self = this;
+	$(this).find('.close').on('click', function() {
+		if ($(self).is('.dialog-giveback')) {
+			if (returnTo) clearTimeout(returnTo);
+			returnTo = false;
+		}
+		self.close();
+	});
+});
+
+function initReturn() {
+	$('.return').each(function() {
+		if ($(this).data('init')) return;
+		$(this).on('click', function() {
+			var id = +$(this).attr('data-id');
+			
+			$.post(location.pathname+location.search, {
+				ajax: 'price',
+				akkuNr: id
+			}, function(data) {
+				if (data) {
+					if (data.success) {
+						var $d = $dialogs.filter('.dialog-giveback'),
+							time = data.time,
+							price = 12 - data.price,
+							$price = $d.find('.price'),
+							$cable = $('[name="kabel-options"]'),
+							$adapterIos = $('[name="ios-options"]'),
+							$adapterUsbc = $('[name="usbc-options"]'),
+							$adapter = $adapterIos.add($adapterUsbc),
+							device = ''+data.device,
+							check = [],
+							checkElements = {
+								cable: $cable,
+								adapterIos: $adapterIos,
+								adapterUsbc: $adapterUsbc
+							},
+							$submit = $d.find('.submit'),
+							$accessories = $cable.add($adapterIos).add($adapterUsbc);
+						$d.find('.accessories').hide();
+						if (device) {
+							$('#cable-wrapper').show();
+							check.push('cable');
+						}
+						if (device == 2) {
+							$('#adapter-ios-wrapper').show();
+							check.push('adapterIos');
+						}
+						if (device == 3) {
+							$('#adapter-usbc-wrapper').show();
+							check.push('adapterUsbc');
+						}
+						$accessories.prop('checked', false).off('.recalculate').on('change.recalculate', function() {
+							var disabled = false;
+							$.each(check, function(k, v) {
+								if (!checkElements[v].filter(':checked').length) disabled = true;
+							});
+							$submit.prop('disabled', disabled);
+							var cable = $cable.filter(':checked').val() == '1' ? 0 : 2,
+								adapter = $adapter.filter(':checked').val() == '1' ? 0 : 1.5;
+							if (device < 2) adapter = 0;
+							else if (!device) {
+								adapter = 0;
+								cable = 0;
+							}
+							var p = Math.ceil((price - cable - adapter) * 100) / 100;
+							if (p < 0) p = 0;
+							$price.text(disabled ? '-' : p.toFixed(2).replace('.', ','));
+						}).trigger('change').parent().each(function() {
+							this.MaterialRadio.checkToggleState();
+						});
+						
+						$submit.prop('disabled', !!check.length);
+						
+						$submit.off('click').on('click', function() {
+							$(this).prop('disabled', true);
+							$.post(location.pathname+location.search, {
+								ajax: 'return',
+								akkuNr: id,
+								time: time,
+								cable: $cable.filter(':checked').val(),
+								adapter: $adapter.filter(':checked').val()
+							}, function(data) {
+								if (data) {
+									if (data.success) {
+										$('tr[data-id="'+id+'"]').remove();
+										historyAdd(data);
+									} else if (data.error) {
+										alert('Es ist ein Fehler aufgetreten: '+data.error);
+									}
+								}
+								if (returnTo) clearTimeout(returnTo);
+								returnTo = false;
+								$d[0].close();
+							});
+							return false;
+						});
+						$d[0].showModal();
+						
+						returnTo = setTimeout(function() {
+							$d[0].close();
+						}, 1000 * 600);
+					}
+				}
+			});
+		});
+		$(this).data('init', true);
+	});
+}
+
+function add(opt) {
+	var $tr = $('<tr data-id="'+opt.id+'" />');
+	var ausgabe = opt.date+"<br>"+opt.time;
+	var kabel = "";
+	switch(opt.device) {
+		case 1:
+			kabel = "Kabel";
+			break;
+		case 2:
+			kabel = "Kabel +\niOS Ad.";
+			break;
+		case 3:
+			kabel = "Kabel +\nUSB-C Ad.";
+			break;
+		case 0:
+			kabel = "Nein";
+	}
+	$tr.append($('<td />').text(opt.id));
+	$tr.append($('<td />').html(ausgabe));
+	$tr.append($('<td />').html(kabel.replace(/\n/g, '<br>')));
+	$tr.append($('<td class="center" />').append($('<a data-id="'+opt.id+'" class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored return" />').html('<i class="material-icons">undo</i>')));
+	$lendTable.find('tbody').append($tr);
+	initReturn();
+}
+
+function historyAdd(opt) {
+	var $tr = $('<tr data-history-id="'+opt.id+'" />');
+	var ausgabe = opt.date+"<br>"+opt.time;
+	var rueckgabe = opt.rdate+"<br>"+opt.rtime;
+	var extra = '';
+	switch (opt.device) {
+		case 1:
+			if (opt.cable == 'no') extra = 'fehlt: K';
+			break;
+		case 2:
+			if (opt.cable == 'no' && opt.adapter == 'no') extra = 'fehlt: K+A';
+			else if (opt.cable == 'no') extra = 'fehlt: K';
+			else if (opt.adapter == 'no') extra = 'fehlt: A';
+			break;
+	}
+	$tr.append($('<td />').text(opt.id));
+	$tr.append($('<td />').html(ausgabe));
+	$tr.append($('<td />').html(rueckgabe));
+	$tr.append($('<td />').html(opt.price+' €'+(extra ? "<br><small>"+extra+'</small>' : '')));
+	$historyTable.find('tbody').prepend($tr);
+}
+
+$('#show-add').on('click', function() {
+	var $d = $dialogs.filter('.dialog-add'),
+		date = new Date(),
+		$devices = $d.find('[name="device"]'),
+		$submit = $d.find('.submit');
+	$d.find('#akkunr').val('').focus();
+	$devices.prop('checked', false).off('change').on('change', function() {
+		if ($(this).is(':checked')) $submit.prop('disabled', false);
+	}).parent().each(function() {
+		this.MaterialRadio.checkToggleState();
+	});
+	$d.find('.info-text').hide();
+	$d.find('.current-time').text(date.getDate()+'.'+(date.getMonth() < 9 ? '0' : '')+(date.getMonth() + 1)+'.'+(''+date.getFullYear()).substr(2)+', '+date.getHours()+':'+(date.getMinutes() < 10 ? '0' : '')+date.getMinutes());
+	$submit.prop('disabled', true).off('click').on('click', function() {
+		var akkuNr = +$d.find('#akkunr').val(),
+			$deviceChecked = $d.find('[name="device"]:checked'),
+			device = +$deviceChecked.val();
+		if (akkuNr && $deviceChecked.length) {
+			$(this).prop('disabled', true);
+			$.post(location.pathname+location.search, {
+				ajax: 'add',
+				akkuNr: akkuNr,
+				device: device
+			}, function(data) {
+				if (data) {
+					if (data.success) {
+						add(data);
+					} else if (data.error) {
+						alert('Es ist ein Fehler aufgetreten: '+data.error);
+					}
+				}
+				$d[0].close();
+			});
+		}
+		return false;
+	});
+	$d[0].showModal();
+});
+
+if (lend) {
+	for (var x in lend) add(lend[x]);
+}
+
+if (hist) {
+	for (var x in hist) historyAdd(hist[x]);
+}
+
+initReturn();
